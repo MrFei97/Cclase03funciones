ef2e8adfe9f4699b8743979fe37e4acf4bdf9426
Fernando Torres <exomfeiyi@gmail.com>
2020-04-06 10:09:16 -0300

    Cambios 1

---
+++ Actividades en clase/Funciones/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Declaracion o prototipo
+void saludar(void);
+
+int main()
+{
+    //llamada o invocacion
+
+    saludar();
+
+    return 0;
+}
+
+void saludar(void)
+{
+   printf("Hola a todos.");
+}
